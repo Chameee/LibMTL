9abc68642dd72d2cf572ce34288096f20ea70469
tangruming <tangruming@bytedance.com>
2023-06-23 22:54:46 +0800

    add mtl and stl code.

--- examples/ins/train_influencer_brand_v3_rep.py
+++ examples/ins/train_influencer_brand_v3_rep.py
@@ -51,10 +51,9 @@ class MTLInfluencer(nn.Module):
         )        
     def forward(self, x):
         x = x.float()
-        accept_head = self.fc1(x)
-        revenue_head = self.fc2(x).mul(accept_head)
-        reputation_head = self.fc3(x).mul(accept_head)
-        return accept_head, revenue_head, reputation_head
+        reputation_head = self.fc1(x)
+
+        return reputation_head
     
     
 
@@ -102,17 +101,17 @@ def main():
             x_batch, y1_batch, y2_batch, y3_batch = batch
             inputs = x_batch.to(device=device)
 
-            accept_label = y1_batch.to(device=device)
-            revenue_label = y2_batch.to(device=device)
+#             accept_label = y1_batch.to(device=device)
+#             revenue_label = y2_batch.to(device=device)
             reputation_label = y3_batch.to(device=device)
 
             optimizer.zero_grad()
-            accept_output, revenue_output, reputation_output = model(inputs)
-            loss_1 = accept_loss(sig(accept_output), accept_label)
-            loss_2 = revenue_loss(sig(revenue_output), revenue_label)
+            reputation_output = model(inputs)
+#             loss_1 = accept_loss(sig(accept_output), accept_label)
+#             loss_2 = revenue_loss(sig(revenue_output), revenue_label)
             loss_3 = reputation_loss(sig(reputation_output), reputation_label)
 
-            loss = loss_1 + loss_2 + loss_3
+            loss = loss_3
             loss.backward()
             optimizer.step()
 
@@ -125,48 +124,48 @@ def main():
     torch.save(model.state_dict(), './saved_model.pkl')
     
     with torch.no_grad():
-        y1_predict_array = []
-        y1_label_array = []
+#         y1_predict_array = []
+#         y1_label_array = []
 
-        y2_predict_array = []
-        y2_label_array = []
+#         y2_predict_array = []
+#         y2_label_array = []
 
         y3_predict_array = []
         y3_label_array = []
         for batch in test_loader:
             x_batch2, y1_batch, y2_batch, y3_batch = batch
             x_batch2 = x_batch2.to(device=device)
-            accept_output, revenue_output, reputation_output = model(x_batch2)
+            reputation_output = model(x_batch2)
 #             predicted_accept = accept_output.argmax(dim=1)
 #             predicted_revenue = revenue_output.argmax(dim=1)
 #             predicted_reputation = reputation_output.argmax(dim=1)
 
 
-            accept_output_cut = [1 if y > 0 else 0 for y in accept_output]
-            revenue_output_cut = [1 if y > 0 else 0 for y in revenue_output]
+#             accept_output_cut = [1 if y > 0 else 0 for y in accept_output]
+#             revenue_output_cut = [1 if y > 0 else 0 for y in revenue_output]
             reputation_output_cut = [1 if y > 0 else 0 for y in reputation_output]
             
-            y1_predict_array.extend(accept_output_cut)
-            y1_label_array.extend(y1_batch.reshape(1,-1).tolist()[0])
+#             y1_predict_array.extend(accept_output_cut)
+#             y1_label_array.extend(y1_batch.reshape(1,-1).tolist()[0])
 
-            y2_predict_array.extend(revenue_output_cut)
-            y2_label_array.extend(y2_batch.reshape(1,-1).tolist()[0])
+#             y2_predict_array.extend(revenue_output_cut)
+#             y2_label_array.extend(y2_batch.reshape(1,-1).tolist()[0])
 
             y3_predict_array.extend(reputation_output_cut)
             y3_label_array.extend(y3_batch.reshape(1,-1).tolist()[0])
 
-        accuracy_accept = accuracy_score(y_true=y1_label_array, y_pred=y1_predict_array)
-        precision_score_accept = precision_score(y_true=y1_label_array, y_pred=y1_predict_array)
-        recall_score_accept = recall_score(y_true=y1_label_array, y_pred=y1_predict_array)
-        f1_score_accept = f1_score(y_true=y1_label_array, y_pred=y1_predict_array)
-        print('y1 Accuracy, precision, recall, f1 of accept: {:.2f} {:.2f} {:.2f} {:.2f}'.format(accuracy_accept, precision_score_accept, recall_score_accept, f1_score_accept))
+#         accuracy_accept = accuracy_score(y_true=y1_label_array, y_pred=y1_predict_array)
+#         precision_score_accept = precision_score(y_true=y1_label_array, y_pred=y1_predict_array)
+#         recall_score_accept = recall_score(y_true=y1_label_array, y_pred=y1_predict_array)
+#         f1_score_accept = f1_score(y_true=y1_label_array, y_pred=y1_predict_array)
+#         print('y1 Accuracy, precision, recall, f1 of accept: {:.2f} {:.2f} {:.2f} {:.2f}'.format(accuracy_accept, precision_score_accept, recall_score_accept, f1_score_accept))
 
 
-        accuracy_revenue = accuracy_score(y_true=y2_label_array, y_pred=y2_predict_array)
-        precision_score_revenue = precision_score(y_true=y2_label_array, y_pred=y2_predict_array)
-        recall_score_revenue = recall_score(y_true=y2_label_array, y_pred=y2_predict_array)
-        f1_score_revenue = f1_score(y_true=y2_label_array, y_pred=y2_predict_array)
-        print('y2 Accuracy, precision, recall, f1 of accept: {:.2f} {:.2f} {:.2f} {:.2f}'.format(accuracy_revenue, precision_score_revenue, recall_score_revenue, f1_score_revenue))
+#         accuracy_revenue = accuracy_score(y_true=y2_label_array, y_pred=y2_predict_array)
+#         precision_score_revenue = precision_score(y_true=y2_label_array, y_pred=y2_predict_array)
+#         recall_score_revenue = recall_score(y_true=y2_label_array, y_pred=y2_predict_array)
+#         f1_score_revenue = f1_score(y_true=y2_label_array, y_pred=y2_predict_array)
+#         print('y2 Accuracy, precision, recall, f1 of accept: {:.2f} {:.2f} {:.2f} {:.2f}'.format(accuracy_revenue, precision_score_revenue, recall_score_revenue, f1_score_revenue))
 
 
         accuracy_reputation = accuracy_score(y_true=y3_label_array, y_pred=y3_predict_array)
@@ -176,4 +175,6 @@ def main():
         print('y3 Accuracy, precision, recall, f1 of accept: {:.2f} {:.2f} {:.2f} {:.2f}'.format(accuracy_reputation, precision_score_reputation, recall_score_reputation, f1_score_reputation))
     
 if __name__ == "__main__":
+    import os 
+    os.environ['CUDA_VISIBLE_DEVICES'] = '4'
     main()
